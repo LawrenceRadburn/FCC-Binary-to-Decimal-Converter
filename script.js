ed797d34f5481e3280cf29604c5fda7336531bef
Lawrence Radburn <lawrence@radnet.ca>
2024-08-28 09:29:50 -0400

    feat: refactor using recursion

--- script.js
+++ script.js
@@ -16,18 +16,11 @@ function countDownAndUp(number) {
 countDownAndUp(3);
 
 const decimalToBinary = (input) => {
-    let binary = "";
-
-    if (binary === 0) {
-        binary = "0";
+    if (input === 0) {
+        return "";
+    } else {
+        return (decimalToBinary(Math.floor(input / 2)) + (input % 2));
     }
-
-    while (input > 0) {
-        binary = (input % 2) + binary;
-        input = Math.floor(input / 2);
-    }
-
-    result.innerText = binary;
 };
 
 
